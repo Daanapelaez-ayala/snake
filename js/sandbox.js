be510dd48fe41c185ec70b136d128341efa79269
daana <126dpelaezayala@frhsd.com>
2025-02-25 14:01:42 -0500

    Message

---
+++ js/sandbox.js
@@ -0,0 +1,9 @@
+let lastRenderTime=0
+const SNAKE_SPEED = 2
+function main(currentTime){
+    window.requestAnimationFrame(main)
+    lastRenderTime=currentTime
+    console.log(secondsSinceLastSRender)
+}
+
+window.requestAnimationFrame(main)
