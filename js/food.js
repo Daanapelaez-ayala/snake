939c3932fcb261421fcfea2484b37ac7763d7c52
daana <126dpelaezayala@frhsd.com>
2025-03-03 14:04:27 -0500

    MessageMessage

---
+++ js/food.js
@@ -0,0 +1,17 @@
+let food = {x: 10, y: 4};
+const EXPANSION_RATE = 1;
+
+export function update (){
+
+}
+export function draw (gameBoard) {
+    const foodElement = document.createElement('div');
+    snakeElement.style.gridRowStart = food. y;
+    snakeElement.style.gridColumnStart = food. x;
+    snakeElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
+
+}
+export  function expandSnake (amount) {
+    newSegments += amoun
+}
--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,5 @@
+import { getInputDirection} from "./input.js";
+
 export const SNAKE_SPEED = 2;
 const snakeBody = [
     { x: 10, y: 11},
@@ -8,12 +10,13 @@ const snakeBody = [
     { x: 15, y: 11}
 ]
 export function update () {
+    const inputDirection = getInputDirection();
     for(let i = snakeBody.length - 2; i >= 0; i--){
         snakeBody[i +1] = { ...snakeBody[i]} ;
     
     }
-   snakeBody[0].x += 1;
-   snakeBody[0].y += 0;
+//    snakeBody[0].x += 1;
+//    snakeBody[0].y += 0;
 
 }
 export function draw(gameBoard) {
